cc2600299616c29b4fab102242ed514e2fa77126
Yehia Elhaw <aldrdaa3663@gmail.com>
2025-05-17 16:56:18 +0200

    test for accessing ride request page

--- SEP-Drive/frontend/src/app/ride/components/ride-page/ride-page.component.ts
+++ SEP-Drive/frontend/src/app/ride/components/ride-page/ride-page.component.ts
@@ -1,4 +1,5 @@
-import {Component} from '@angular/core';
+import {Component, OnInit} from '@angular/core';
+import {AuthService} from '../../../auth/auth.service';
 
 @Component({
   selector: 'app-ride-page',
@@ -6,5 +7,25 @@ import {Component} from '@angular/core';
   templateUrl: './ride-page.component.html',
   styleUrl: './ride-page.component.scss'
 })
-export class RidePageComponent {
+export class RidePageComponent implements OnInit {
+  username: string = '';
+  accessAllowed: boolean = false;
+
+  constructor(private authService: AuthService) {}
+
+  ngOnInit() {
+    this.authService.currentUser.subscribe({
+      next: user => {
+        if (user) {
+          if (!user.username) return;
+          this.username = user.username;
+          this.authService.isCustomer(this.username).subscribe({
+            next: isCustomer => this.accessAllowed = isCustomer,
+            error: err => console.log(err),
+          });
+        }
+      },
+      error: err => console.log(err)
+    });
+  }
 }
