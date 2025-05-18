c4f9cdb9645e46b89ad62d931a2ee5949c091864
Yehia Elhaw <aldrdaa3663@gmail.com>
2025-05-19 01:34:59 +0200

    Ride requests with specific coordinates now viable

--- SEP-Drive/frontend/src/app/ride/components/location-autocomplete/location-autocomplete.component.ts
+++ SEP-Drive/frontend/src/app/ride/components/location-autocomplete/location-autocomplete.component.ts
@@ -1,5 +1,5 @@
 import {Component, EventEmitter, Input, OnInit, Output} from '@angular/core';
-import {FormControl} from '@angular/forms';
+import {FormControl, Validators} from '@angular/forms';
 import {Observable, of} from 'rxjs';
 import {debounceTime, distinctUntilChanged, map, switchMap, catchError} from 'rxjs/operators';
 import {PlacesService} from '../../services/places.service';
@@ -17,6 +17,19 @@ export class LocationAutocompleteComponent implements OnInit {
   @Input() control!: FormControl;
   @Output() locationSelected = new EventEmitter<Location>();
   @Output() pinClicked = new EventEmitter<void>();
+
+  manualMode = false;
+  latitude = new FormControl<number | null>(null, [
+    Validators.required,
+    Validators.min(-90),
+    Validators.max(90)
+  ]);
+
+  longitude = new FormControl<number | null>(null, [
+    Validators.required,
+    Validators.min(-180),
+    Validators.max(180)
+  ]);
 
   filteredLocations!: Observable<Location[]>;
 
@@ -46,5 +59,22 @@ export class LocationAutocompleteComponent implements OnInit {
 
   onLocationSelected(location: Location) {
     this.locationSelected.emit(location);
+  }
+
+  toggleManualMode(): void {
+    this.manualMode = !this.manualMode;
+  }
+
+  useManualCoordinates(): void {
+    if (this.latitude.valid && this.longitude.valid) {
+      const loc: Location = {
+        latitude: this.latitude.value!,
+        longitude: this.longitude.value!,
+        name: 'Manual Location',
+      };
+      this.control.setValue(loc);
+      this.locationSelected.emit(loc);
+      this.manualMode = false;
+    }
   }
 }
