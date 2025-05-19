3471077d6014dab0bb3777ac5b92d17704baa9ea
Abdallah Abouelmagd <abdallah.abouelmagd@stud.uni-due.de>
2025-05-19 20:11:05 +0200

    Data base converted to persistent, yml created

--- SEP-Drive/Backend/src/main/java/com/example/sep_drive_backend/models/JwtTokenProvider.java
+++ SEP-Drive/Backend/src/main/java/com/example/sep_drive_backend/models/JwtTokenProvider.java
@@ -5,7 +5,9 @@ import io.jsonwebtoken.JwtException;
 import io.jsonwebtoken.Jwts;
 import io.jsonwebtoken.SignatureAlgorithm;
 import io.jsonwebtoken.security.Keys;
+import jakarta.annotation.PostConstruct;
 import jakarta.servlet.http.HttpServletRequest;
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.stereotype.Component;
 
 import javax.crypto.SecretKey;
@@ -15,12 +17,17 @@ import java.util.Date;
 @Component
 public class JwtTokenProvider {
 
-    private final String secretKeyString = "superdupersecretkey324chasdasfwerqwre"; // 32+ characters
-    private final SecretKey secretKey = Keys.hmacShaKeyFor(secretKeyString.getBytes(StandardCharsets.UTF_8));
+    @Value("${JWT_SECRET}")
+    private String secretKeyString;
+
+    private SecretKey secretKey;
 
     private final long validityInMilliseconds = 3600000; // 1 hour
 
-
+    @PostConstruct
+    public void init() {
+        this.secretKey = Keys.hmacShaKeyFor(secretKeyString.getBytes(StandardCharsets.UTF_8));
+    }
 
     public String createToken(String username) {
         Date now = new Date();
