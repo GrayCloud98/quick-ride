86bf022f7c6538d022a21fa5ab68e2f1eee058e9
Abdallah Abouelmagd <abdallah.abouelmagd@stud.uni-due.de>
2025-06-06 19:19:20 +0200

    allowed cors origin  for ws testing

--- SEP-Drive/Backend/src/main/java/com/example/sep_drive_backend/configuration/WebSocketConfig.java
+++ SEP-Drive/Backend/src/main/java/com/example/sep_drive_backend/configuration/WebSocketConfig.java
@@ -18,7 +18,9 @@ public class WebSocketConfig implements WebSocketMessageBrokerConfigurer {
 
     @Override
     public void registerStompEndpoints(StompEndpointRegistry registry) {
-        registry.addEndpoint("/ws").setAllowedOrigins("*").withSockJS(); // enable SockJS fallback
+        registry.addEndpoint("/ws")
+                .setAllowedOrigins("http://localhost:4200", "http://localhost:63342")
+                .withSockJS();
     }
 }
 
