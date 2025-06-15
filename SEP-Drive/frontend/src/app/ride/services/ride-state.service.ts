73f17292bb37430f47d0e66c90f354905f0806e7
marwantaher <somatahe@stud.uni-due.de>
2025-06-15 17:01:29 +0200

    i did the simulation and ranking code

--- SEP-Drive/frontend/src/app/ride/services/ride-state.service.ts
+++ SEP-Drive/frontend/src/app/ride/services/ride-state.service.ts
@@ -5,28 +5,19 @@ import { BehaviorSubject } from 'rxjs';
 providedIn: 'root'
 })
 export class RideStateService {
+// Holds the latest pickup and dropoff
 private pickupLocationSubject = new BehaviorSubject<{ lat: number, lng: number } | null>(null);
 private dropoffLocationSubject = new BehaviorSubject<{ lat: number, lng: number } | null>(null);
-private currentRideIdSubject = new BehaviorSubject<string | null>(null);
 
 pickupLocation$ = this.pickupLocationSubject.asObservable();
 dropoffLocation$ = this.dropoffLocationSubject.asObservable();
-currentRideId$ = this.currentRideIdSubject.asObservable();
 
-get currentRideId(): string | null {
-    return this.currentRideIdSubject.value;
-  }
-
-  setPickupLocation(location: { lat: number, lng: number } | null) {
+setPickupLocation(location: { lat: number, lng: number } | null) {
     this.pickupLocationSubject.next(location);
   }
 
   setDropoffLocation(location: { lat: number, lng: number } | null) {
     this.dropoffLocationSubject.next(location);
-  }
-
-  setCurrentRideId(id: string) {
-    this.currentRideIdSubject.next(id);
   }
 
   resetLocations() {
