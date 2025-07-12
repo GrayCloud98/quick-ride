6d836810002999a5442249e236aa76109700cdbd
spmokhal <mohamed.khalfallah@stud.uni-due.de>
2025-07-12 02:21:20 +0200

    "unit test for transfer"

--- SEP-Drive/Backend/src/main/java/com/example/sep_drive_backend/repository/WalletRepository.java
+++ SEP-Drive/Backend/src/main/java/com/example/sep_drive_backend/repository/WalletRepository.java
@@ -5,4 +5,7 @@ import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
 
 @Repository
-public interface WalletRepository extends JpaRepository<Wallet, Long> { }
+public interface WalletRepository extends JpaRepository<Wallet, Long> {
+    Wallet findByUsername(String username);
+    Wallet findByUserId(Long id);
+}
