0ebc93b94394483ae507770517d3b440a77c40c8
Ziad Morsy <ziad.morsy1998@hotmail.com>
2025-07-15 16:04:17 +0200

    old .tar replaced

--- SEP-Drive/Backend/src/main/java/com/example/sep_drive_backend/configuration/WebSocketConfig.java
+++ SEP-Drive/Backend/src/main/java/com/example/sep_drive_backend/configuration/WebSocketConfig.java
@@ -20,7 +20,7 @@ public class WebSocketConfig implements WebSocketMessageBrokerConfigurer {
     @Override
     public void registerStompEndpoints(StompEndpointRegistry registry) {
         registry.addEndpoint("/ws")
-                .setAllowedOrigins("http://localhost:4200", "http://localhost:63342","http://localhost:5500")
+                .setAllowedOriginPatterns("*")
                 .withSockJS();
     }
 
