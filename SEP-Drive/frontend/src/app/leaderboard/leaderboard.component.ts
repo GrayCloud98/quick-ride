5823b6b64535fa9c48e333b52154b4e2640b3bd1
marwantaher <somatahe@stud.uni-due.de>
2025-07-15 02:37:22 +0200

    Done with design fola l fola

--- SEP-Drive/frontend/src/app/leaderboard/leaderboard.component.ts
+++ SEP-Drive/frontend/src/app/leaderboard/leaderboard.component.ts
@@ -5,7 +5,7 @@ import { LeaderboardService, DriverBoard } from './leaderboard.service';
   selector: 'app-leaderboard',
   standalone: false,
   templateUrl: './leaderboard.component.html',
-  styleUrls: ['./leaderboard.component.scss']  // <-- Note: 'styleUrls' not 'styleUrl'
+  styleUrls: ['./leaderboard.component.scss']
 })
 export class LeaderboardComponent implements OnInit {
 
@@ -32,7 +32,7 @@ export class LeaderboardComponent implements OnInit {
 
   sortBy(column: keyof DriverBoard): void {
     if (this.currentSortColumn === column) {
-      // Toggle direction if the same column is clicked again
+
       this.sortDirection = this.sortDirection === 'asc' ? 'desc' : 'asc';
     } else {
       this.sortDirection = 'asc';
@@ -43,7 +43,7 @@ export class LeaderboardComponent implements OnInit {
       let valueA = a[column];
       let valueB = b[column];
 
-      // Convert to lowercase if strings (for case-insensitive sorting)
+
       if (typeof valueA === 'string' && typeof valueB === 'string') {
         valueA = valueA.toLowerCase();
         valueB = valueB.toLowerCase();
